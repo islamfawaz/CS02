fa7feff5b5fbb04a66dded6c74b8cfcf8b4d1bcc
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-06-25 23:05:14 +0200

    question13

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -170,7 +170,12 @@
             //DateTime date = new DateTime(2024, 6, 14); 
             //Console.WriteLine($"The event is on {date:MM/dd/yyyy}");// The event is on 06/14/2024
 
-           
+            //13 Which of the following statements is correct about the C#.NET code snippet given below?
+
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //A value 1 will be assigned to d.
+
 
 
 
