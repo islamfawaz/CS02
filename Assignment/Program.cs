229c173862da51ce844dd119808126b5f85edc5e
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-06-25 22:01:14 +0200

    question1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace Assignment
 {
+    class point
+    {
+        public int x;
+        public int y;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1 Write a program that allows the user to enter a number then print it.
+            #region question1
+             int number = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(number);
+            #endregion
         }
     }
 }
