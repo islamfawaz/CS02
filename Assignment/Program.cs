5b692a3f6125a1e341d3449bfb3730c1a08f5fc4
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-06-25 22:03:05 +0200

    question2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -15,6 +15,21 @@
             
             Console.WriteLine(number);
             #endregion
+            //2 Write C# program that Convert a string to an integer,
+            //but the string contains non-numeric characters.And mention what will happen 
+            #region question2
+            //String input = "2as2dsd43";
+            //int.TryParse(input, out int result);
+            //if (result==0)
+            //{
+            //    Console.WriteLine("cant be convert");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(result);
+            //}
+            #endregion
+
         }
     }
 }
